de30303312ed4bb9dd545e36ec06aebd667391a0
Gabrielle Lima <67937973+gabizinha12@users.noreply.github.com>
2020-10-29 22:51:54 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,12 @@
+// this file is read only, you cannot write it
+var usuario1 = ['Et da Estônia', 17, 170]
+var usuario2 = ['Pessoa do Pântano', 39, 198]
+var usuario3 = ['Homem da Lua Virada', 21, 149]
+var usuario4 = ['Pequena Paulistana', 18, 171]
+var usuario5 = ['Menino da Porteira', 13, 142]
+function maiorAlto(usuario) {
+    if(usuario[1] >= 18 && usuario[2] >= 170) {
+        return true 
+    }
+    return false
+}
