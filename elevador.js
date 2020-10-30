2548eec6c708b5dfa1fcbcc82e0f6a60e3f03190
Gabrielle Lima <67937973+gabizinha12@users.noreply.github.com>
2020-10-29 22:51:33 -0300

    Create elevador.js

---
+++ elevador.js
@@ -0,0 +1,32 @@
+// this file is read only, you cannot write it
+var moradores = [
+
+   "Fulano de Tal",
+
+   "Beltrano da Cia",
+
+   "Viajante do Tempo",
+
+   "Morador da Lua",
+
+   "Marciano Azul",
+
+   "Et da Eslováquia",
+
+   "Jedi do Lado Cinza da Força",
+
+   "Baby Yoda Amarelo"
+
+]
+
+   for(i=0; i<moradores.length; i++){
+
+      if(i%2==0){
+
+           console.log("O morador " + moradores[i] + " pode usar o elevador")        
+
+   }
+
+   
+
+}
