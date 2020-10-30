d63afda1fd8fbf69e7f0afbf67006ef4bce62df5
Gabrielle Lima <67937973+gabizinha12@users.noreply.github.com>
2020-10-29 22:52:45 -0300

    Create estacionamento.js

---
+++ estacionamento.js
@@ -0,0 +1,41 @@
+// this file is read only, you cannot write it
+// Esse array é utilizado dentro das funções. Cada placa neste array representa
+// uma entrada do respectivo veículo no estacionamento. Não é necessário alterar
+// esse array.
+var placas = [
+   'RXB-2525', 'AKX-3333', 'ORO-7142','RXB-2525', 'AKX-3333', 'ORO-7142',
+   'AKX-3333', 'RXB-2525', 'AKX-3333','AKX-3333', 'RXB-2525', 'AKX-3333',   'RXB-2525', 'AKX-3333', 'ORO-7142','AKX-3333', 'AKX-3333', 'RXB-2525',
+   'AKX-3333', 'ORO-7142', 'ORO-7142','AKX-3333', 'AKX-3333', 'RXB-2525',
+   'AKX-3333', 'AKX-3333', 'RXB-2525','AKX-3333', 'AKX-3333', 'RXB-2525',
+   'AKX-3333', 'ORO-7142', 'ORO-7142','AKX-3333', 'ORO-7142', 'ORO-7142',
+   'ORO-7142', 'RXB-2525', 'AKX-3333','AKX-3333', 'ORO-7142', 'ORO-7142',
+   'AKX-3333', 'RXB-2525', 'AKX-3333','AKX-3333', 'RXB-2525', 'AKX-3333',
+   'RXB-2525', 'AKX-3333', 'ORO-7142','AKX-3333', 'AKX-3333', 'RXB-2525',
+   'AKX-3333', 'ORO-7142', 'ORO-7142','AKX-3333', 'AKX-3333', 'RXB-2525',
+   'AKX-3333', 'AKX-3333', 'RXB-2525','AKX-3333', 'AKX-3333', 'RXB-2525'
+]
+
+function calcularNumeroDeEntradas(placa){
+   // Seu código vem aqui...
+   var numeroEntradas = 0;
+   for(var i = 0; i < placas.length; i++) {
+      if(placa == placas[i]) {
+         numeroEntradas++
+      }
+      return numeroEntradas;
+   }
+}
+ 
+function calcularValorDevido(placa){
+    var nEntradas = calcularNumeroDeEntradas(placa)
+    var valorTotal = 0
+   // Seu código vem aqui...
+   if(nEntradas <= 20) {
+      valorTotal = nEntradas * 10
+      return valorTotal
+   } else {
+      valorTotal = ((nEntradas - 20) * 5) + 200
+      return valorTotal
+   }
+}
+
